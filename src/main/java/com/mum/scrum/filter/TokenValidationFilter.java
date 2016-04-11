58538cae3ffb175d8b4fceeef84c09f235e41c50
nadimcse <nadim_csedu@yahoo.com>
2016-04-11 01:13:21 -0500

    code refactored

--- src/main/java/com/mum/scrum/filter/TokenValidationFilter.java
+++ src/main/java/com/mum/scrum/filter/TokenValidationFilter.java
@@ -10,10 +10,12 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Properties;
 
 /**
@@ -65,8 +67,10 @@ public class TokenValidationFilter implements Filter {
             String token = request.getParameter("token");
             if (!Utility.authenticateToken(token, prop.getProperty("token.secret.key"))) {
                 response.setContentType("application/json");
+                ((HttpServletResponse)response).setStatus(HttpServletResponse.SC_FORBIDDEN);
                 PrintWriter out = response.getWriter();
-                out.print(mapper.writeValueAsString("goto login page"));//TODO will add json object
+                out.print(mapper.writeValueAsString(Utility.populateViewModel(
+                        Utility.NOT_LOGGED_IN_STATUS_CODE, Arrays.asList("Not logged in"))));//TODO will add json object
                 out.flush();
                 return;
             }
